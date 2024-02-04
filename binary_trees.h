8703109b8ee71ee745966f9f1dbdda01761548be
root <root@ubuntu.myguest.virtualbox.org>
2024-02-03 23:04:22 -1200

    fixes

--- binary_trees.h
+++ binary_trees.h
@@ -20,16 +20,14 @@
  *
 */
 
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    /* data */
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
